ddb75eb3d698fd2d0d72909a1ba028d98a30175c
Ivo de Vero <2310555d@student.gla.ac.uk>
2020-12-30 10:48:08 +0100

    create static case page

--- src/serious-historical-game/src/components/MyCase.jsx
+++ src/serious-historical-game/src/components/MyCase.jsx
@@ -8,10 +8,17 @@ class MyCase extends Component {
     return (
       <>
         <h1>My Case</h1>
-        <div className="mydocuments-case">
+        <h3>case question</h3>
+        <div className="mydocuments-mycase">
           <MyDocuments></MyDocuments>
         </div>
-        <div>
+        <div className="headings-mycase">
+          <h4>Challenge</h4>
+          <h4>Defend</h4>
+          <h4>Focus</h4>
+          <button>Present your case</button>
+        </div>
+        <div className="closebutton-mycase">
           <CloseButton></CloseButton>
         </div>
       </>
